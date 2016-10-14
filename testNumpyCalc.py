5b8152dac42445cd196e3c5759c7c298c8711ffb
sugarArt <blue.sugarart@gmail.com>
2016-10-14 12:43:27 +0900

    numpy計算の練習用コードの作成。

---
+++ testNumpyCalc.py
@@ -0,0 +1,20 @@
+#!user/bin/python
+
+# testProgram.py
+# Created on 2016/10/14
+# Copyright(c) by.Suga Arata 
+
+# For Test calculation in numpy array
+
+
+import math
+import numpy as np
+#import matplotlib.pyplot as plt
+#import matplotlib as mpl
+#import matplotlib.animation as animation
+
+
+
+
+
+
