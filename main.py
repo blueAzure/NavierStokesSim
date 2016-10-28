04bcaffcadafa613e4d13cc246cba64fe28369ab
sugarArt <blue.sugarart@gmail.com>
2016-10-28 10:47:28 +0900

    start creating the skelton of main simulation & visuzalization code

---
+++ main.py
@@ -0,0 +1,60 @@
+#!usr/bin/python
+
+"""
+That is RayleighBenardConvectionSim.py
+
+
+
+"""
+
+import math
+import numpy as np
+
+
+# solvePoissonEquation
+# that calls poissonLoop
+def solvePoissonEquation():
+    print solvePoisoonEquation
+    # init velocity temp Array for update
+    # init pressure temp Array for update
+    # loop: 
+    #   call modPhi
+    #   check error
+    # endLoop
+
+
+# modPhi: modify Phi number.
+# Phi is modificated Pressure
+# that is called "solvePoissonEquation" 
+def modPhi():
+    print "!"
+
+
+def updateFields():
+    print "updateFields!"
+
+    # update Pressure
+    # update velocity(u,v)
+    # check solution by Contiunous Equation
+
+
+if __name__ == '__main__':
+    print "start Program!"
+    
+    # Initialize Fields
+    # Initialize Plot
+
+    # loop:
+    #   update field
+    #   (error check)
+    #   update plot
+    #   save image
+    # endLoop
+
+    print "end Program!"
+ 
+
+
+
+
+
